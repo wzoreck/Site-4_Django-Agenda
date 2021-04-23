dba9e49e84cde455afd3d3013d0d2d0ab30586d7
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-04-23 08:54:37 -0300

    Forma mais simplificada para gerar 404

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 # Este é um atalho para não ter que repitir o código toda vez no try/except
 from django.http import Http404
 from .models import Contato
 
@@ -8,8 +8,6 @@ def index(request):
     return render(request, 'contatos/index.html', {'contatos' : contatos})
 
 def ver_contato(request, contato_id): # O argumento passado pela URL vai ser passado para a view
-    try:
-        contato = Contato.objects.get(id=contato_id)
-        return render(request, 'contatos/ver_contato.html', {'contato' : contato})
-    except Contato.DoesNotExist as e:
-        raise Http404() # Caso o id do contato passado não exista, erro 404
+    # contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id) # Forma para obter o objeto ou gerar 404
+    return render(request, 'contatos/ver_contato.html', {'contato' : contato})
