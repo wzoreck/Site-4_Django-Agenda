cddfbaf68191333b4b4a6741446114199144fc6d
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-04-23 09:41:43 -0300

    Paginaçao adicionada a tabela de contatos

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404 # Este é um atalho para não ter que repitir o código toda vez no try/except
 from django.http import Http404
 from .models import Contato
+from django.core.paginator import Paginator # Para criar paginação em listas/tabelas com vários dados
 
 # Create your views here.
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 2)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {'contatos' : contatos})
 
 def ver_contato(request, contato_id): # O argumento passado pela URL vai ser passado para a view
